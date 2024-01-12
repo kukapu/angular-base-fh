e8cf129b93b220960366db77487acbd4ad0306a1
Albert Alvarez Estelles <114157589+kukapu@users.noreply.github.com>
2024-01-12 09:31:47 +0100

    04- create appCountry

---
+++ 04-countryApp/src/app/shared/pages/aboutPage/aboutPage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-about-page',
+  templateUrl: './aboutPage.component.html',
+  styles: `
+    :host {
+      display: block;
+    }
+  `,
+})
+export class AboutPageComponent { }
